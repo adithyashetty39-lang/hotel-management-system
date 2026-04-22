6c784cf1930142b67d7ed4ad986f78f41c1adb25
Adithya B Shetty <adithyashetty39@gmail.com>
2026-04-22 10:21:36 +0530

    db.js chnage

--- config/db.js
+++ config/db.js
@@ -1,19 +1,11 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306, // <-- Added this line
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-pool.getConnection()
-    .then(() => console.log('✅ MySQL Database Connected Successfully!'))
-    .catch((err) => console.error('❌ Database Connection Failed:', err.message));
-
-module.exports = pool;
+module.exports = db;
